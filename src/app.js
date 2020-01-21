44563b7148de5376e10170690987838ced3dd316
thothdragon <thothdragons@gmail.com>
2020-01-21 15:05:07 +0100

    Evolved architecture

--- src/app.js
+++ src/app.js
@@ -1,8 +1,1 @@
-import "materialize-css";
 import { City } from "./app/shared/models/city";
-import { Weather } from "./app/shared/models/weather";
-import { Pollution } from "./app/shared/models/pollution";
-
-console.log(new City);
-
-
