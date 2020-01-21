8d289e0126ed6ce0440505a41adaadf3a47482b6
thothdragon <thothdragons@gmail.com>
2020-01-21 13:02:55 +0100

    Initialize Models cover

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
 import "materialize-css";
+import { City } from "./shared/models/city";
+import { Weather } from "./shared/models/weather";
+import { Pollution } from "./shared/models/pollution";
+
+console.log(new City);
+
+
