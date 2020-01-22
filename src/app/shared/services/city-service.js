99cbc9ec86cd8917a47c052030689ee60f920a15
thothdragon <thothdragons@gmail.com>
2020-01-22 17:13:35 +0100

    Fix render in Navbar & prepare ServiceWeather in CityService

--- src/app/shared/services/city-service.js
+++ src/app/shared/services/city-service.js
@@ -20,8 +20,6 @@ export class CityService {
         xhr.open(`GET`, `https://api.openweathermap.org/data/2.5/weather?appid=e07ec698e7a0cff9443b2c91b5b6eca6&units=metric&q=${cityName}`);
         xhr.setRequestHeader(`Accept`, `application/json`);
         xhr.onload = () => {
-            console.log(cityName);
-            
             const json = JSON.parse(xhr.response);
             this.city.weather.description = json.weather[0].main;
             this.city.weather.humidity = json.main.humidity;
